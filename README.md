c43a84df20c37345692f27aee3d2f572fc237691
blueJpg <2238288979@qq.com>
2021-12-06 01:07:05 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+
+
+#### 参考
+- [cmake 添加头文件目录，链接动态、静态库](https://www.cnblogs.com/binbinjx/p/5626916.html)
+- [CMake 心得与实用技巧](https://zhuanlan.zhihu.com/p/429404175)
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "add.h"
+#include "max.h"
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    cout << add(1, 1) << endl;
+    cout << max(1, 2) << endl;
+    return 0;
+}
